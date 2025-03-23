7a5fd01b7ca70d8c7d009af7156a35aaa7d024eb
Trevor Karuma <trevormukundi@gmail.com>
2025-03-23 14:54:39 +0300

    changes

---
+++ Challenges/speed-detector.js
@@ -0,0 +1,16 @@
+function speedDetector(speed) {
+    const speedLimit = 70;
+    const kmPerDemeritPoint = 5;
+    const maxDemeritPoints = 12;
+
+    if (speed < speedLimit) {
+        console.log("Ok");
+    } else {
+        let demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
+        if (demeritPoints > maxDemeritPoints) {
+            console.log("License suspended");
+        } else {
+            console.log(`Points: ${demeritPoints}`);
+        }
+    }
+}
